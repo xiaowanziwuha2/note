4272844456ab63d1b748783254fe86a3a86abe69
xiaowanziwuha2 <89444444@163.com>
2020-04-03 16:42:01 +0800

    add copy picture function

--- src/main/java/com/test/demo/demo/ExcelTemplateUtils.java
+++ src/main/java/com/test/demo/demo/ExcelTemplateUtils.java
@@ -1,5 +1,6 @@
 package com.test.demo.demo;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -29,75 +30,73 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.core.io.ClassPathResource;
 
 public class ExcelTemplateUtils {
-	public static void process(Object data, String templatePath, OutputStream os) throws Exception {
-	    if (data == null || StringUtils.isEmpty(templatePath)) {
-	        return;
-	    }
-	    try {
-	        ClassPathResource resource = new ClassPathResource(templatePath);
-			InputStream inputStream = resource.getInputStream();
-			Workbook wb = WorkbookFactory.create(inputStream);
-	        
-	        Iterator<Sheet> iterable = wb.sheetIterator();
-	        while (iterable.hasNext()) {
-	            processSheet(data, iterable.next());
-	        }
-	        wb.write(os);
-	    } catch (IOException | InvalidFormatException e) {
-	        e.printStackTrace();
-	    }
+	public static byte[] process(Object data, String templatePath) throws Exception {
+		if (data == null || StringUtils.isEmpty(templatePath)) {
+			return null;
+		}
+		ClassPathResource resource = new ClassPathResource(templatePath);
+		InputStream inputStream = resource.getInputStream();
+		Workbook wb = WorkbookFactory.create(inputStream);
+
+		/*
+		 * Iterator<Sheet> iterable = wb.sheetIterator(); while (iterable.hasNext()) {
+		 * processSheet(data, iterable.next()); }
+		 */
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		wb.write(out);
+		return out.toByteArray();
 	}
-	
-	private static void processSheet(Object data, Sheet sheet) throws Exception{
-	    Map<Integer, Map<Integer, Cell>> listRecord = new LinkedHashMap<>();
-	    int lastRowNum = sheet.getLastRowNum();
-	    for (int i = lastRowNum; i >= 0; i--) {
-	        Row row = sheet.getRow(i);
-	        if (row == null) {
-	            continue;
-	        }
-	        int lastCellNum = row.getLastCellNum();
-	        for (int j = 0; j < lastCellNum; j++) {
-	            Cell cell = row.getCell(j);
-	            if (cell == null) {
-	                continue;
-	            }
-	            try {
-	                String cellValue = cell.getStringCellValue();
-	                if (cellValue.matches(".*\\$\\{[\\w.()]+}.*")) {//${cls.headmaster}
-	                    fillCell(cell, cellValue, data);
-	                } else if (cellValue.matches(".*\\$\\{[\\w.]+\\[#][\\w.]+}.*")) {//${cls.students[#].name}
-	                    Map<Integer, Cell> rowRecord = listRecord.computeIfAbsent(i, k -> new HashMap<>());
-	                    rowRecord.put(j, cell);
-	                }
-	            } catch (Exception e) {
-	            	System.out.println("Error Log...");
-	            }
-	        }
-	    }
-	    
-	    Map<String, List> listInData = new HashMap<>();
-	    Map<String, CellStyle> listCellStyle = new HashMap<>();
-	    Map<Cell, String> listCellPath = new HashMap<>();
-	    listRecord.forEach((rowNum, colMap) -> {
-	        Pattern p = Pattern.compile("\\$\\{[\\w.\\[#\\]]+}");
-	        Set<String> listPath = new HashSet<>();
-	        colMap.forEach((colNum, cell) -> {
-	            String cellValue = cell.getStringCellValue();
-	            Matcher m = p.matcher(cellValue);
-	            if (m.find()) {
-	                String reg = m.group();
-	                String regPre = reg.substring(2, reg.indexOf("["));
-	                String regSuf = reg.substring(reg.lastIndexOf("].") + 2, reg.length() - 1);
-	                listPath.add(regPre);
-	                listCellStyle.put(String.format("%s.%s", regPre, regSuf), cell.getCellStyle());
-	                listCellPath.put(cell, String.format("%s#%s", regPre, regSuf));
-	            }
-	        });
-	        
-	        int maxRow = 0;
-	        for (String path : listPath) {
-	            Object list = null;
+
+	private static void processSheet(Object data, Sheet sheet) throws Exception {
+		Map<Integer, Map<Integer, Cell>> listRecord = new LinkedHashMap<>();
+		int lastRowNum = sheet.getLastRowNum();
+		for (int i = lastRowNum; i >= 0; i--) {
+			Row row = sheet.getRow(i);
+			if (row == null) {
+				continue;
+			}
+			int lastCellNum = row.getLastCellNum();
+			for (int j = 0; j < lastCellNum; j++) {
+				Cell cell = row.getCell(j);
+				if (cell == null) {
+					continue;
+				}
+				try {
+					String cellValue = cell.getStringCellValue();
+					if (cellValue.matches(".*\\$\\{[\\w.()]+}.*")) {// ${cls.headmaster}
+						fillCell(cell, cellValue, data);
+					} else if (cellValue.matches(".*\\$\\{[\\w.]+\\[#][\\w.]+}.*")) {// ${cls.students[#].name}
+						Map<Integer, Cell> rowRecord = listRecord.computeIfAbsent(i, k -> new HashMap<>());
+						rowRecord.put(j, cell);
+					}
+				} catch (Exception e) {
+					System.out.println("Error Log...");
+				}
+			}
+		}
+
+		Map<String, List> listInData = new HashMap<>();
+		Map<String, CellStyle> listCellStyle = new HashMap<>();
+		Map<Cell, String> listCellPath = new HashMap<>();
+		listRecord.forEach((rowNum, colMap) -> {
+			Pattern p = Pattern.compile("\\$\\{[\\w.\\[#\\]]+}");
+			Set<String> listPath = new HashSet<>();
+			colMap.forEach((colNum, cell) -> {
+				String cellValue = cell.getStringCellValue();
+				Matcher m = p.matcher(cellValue);
+				if (m.find()) {
+					String reg = m.group();
+					String regPre = reg.substring(2, reg.indexOf("["));
+					String regSuf = reg.substring(reg.lastIndexOf("].") + 2, reg.length() - 1);
+					listPath.add(regPre);
+					listCellStyle.put(String.format("%s.%s", regPre, regSuf), cell.getCellStyle());
+					listCellPath.put(cell, String.format("%s#%s", regPre, regSuf));
+				}
+			});
+
+			int maxRow = 0;
+			for (String path : listPath) {
+				Object list = null;
 				try {
 					list = getAttributeByPath(data, path);
 				} catch (Exception e) {
@@ -105,101 +104,105 @@ public class ExcelTemplateUtils {
 					e.printStackTrace();
 					throw new RuntimeException(e);
 				}
-	            
+
 				if (list == null) {
-	                list = new ArrayList<>();
-	            }
-	            
-	            if (list instanceof List) {
-	                int len = ((List) list).size();
-	                maxRow = maxRow > len ? maxRow : len;
-	                listInData.put(path, ((List) list));
-	            } else {
-	                throw new IllegalArgumentException(String.format("%s is not a list but a %s", path, list.getClass().getSimpleName()));
-	            }
-	        }
-	        if (maxRow > 1) {
-	            int endRow = sheet.getLastRowNum();
-	            sheet.shiftRows(rowNum + 1, endRow + 1, maxRow - 1);
-	        }
-	    });
-	    
-	    listRecord.forEach((rowNum, colMap) -> {
-	        colMap.forEach((colNum, cell) -> {
-	            String path = listCellPath.get(cell);
-	            String[] pathData = path.split("#");
-	            List list = listInData.get(pathData[0]);
-	            int baseRowIndex = cell.getRowIndex();
-	            int colIndex = cell.getColumnIndex();
-	            CellStyle style = listCellStyle.get(String.format("%s.%s", pathData[0], pathData[1]));
-	            for (int i = 0; i < list.size(); i++) {
-	                int rowIndex = baseRowIndex + i;
-	                Row row = sheet.getRow(rowIndex);
-	                if (row == null) {
-	                    row = sheet.createRow(rowIndex);
-	                }
-	                Cell cellToFill = row.getCell(colIndex);
-	                if (cellToFill == null) {
-	                    cellToFill = row.createCell(colIndex);
-	                }
-	                cellToFill.setCellStyle(style);
-	                try {
+					list = new ArrayList<>();
+				}
+
+				if (list instanceof List) {
+					int len = ((List) list).size();
+					maxRow = maxRow > len ? maxRow : len;
+					listInData.put(path, ((List) list));
+				} else {
+					throw new IllegalArgumentException(
+							String.format("%s is not a list but a %s", path, list.getClass().getSimpleName()));
+				}
+			}
+			if (maxRow > 1) {
+				int endRow = sheet.getLastRowNum();
+				sheet.shiftRows(rowNum + 1, endRow + 1, maxRow - 1);
+			}
+		});
+
+		listRecord.forEach((rowNum, colMap) -> {
+			colMap.forEach((colNum, cell) -> {
+				String path = listCellPath.get(cell);
+				String[] pathData = path.split("#");
+				List list = listInData.get(pathData[0]);
+				int baseRowIndex = cell.getRowIndex();
+				int colIndex = cell.getColumnIndex();
+				CellStyle style = listCellStyle.get(String.format("%s.%s", pathData[0], pathData[1]));
+				for (int i = 0; i < list.size(); i++) {
+					int rowIndex = baseRowIndex + i;
+					Row row = sheet.getRow(rowIndex);
+					if (row == null) {
+						row = sheet.createRow(rowIndex);
+					}
+					Cell cellToFill = row.getCell(colIndex);
+					if (cellToFill == null) {
+						cellToFill = row.createCell(colIndex);
+					}
+					cellToFill.setCellStyle(style);
+					try {
 						setCellValue(cellToFill, getAttribute(list.get(i), pathData[1]));
 					} catch (Exception e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 						throw new RuntimeException(e);
-					} 
-	            }
-	        });
-	    });
+					}
+				}
+			});
+		});
 	}
 
-	private static void fillCell(Cell cell, String expression, Object data) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
-	    Pattern p = Pattern.compile("\\$\\{[\\w.\\[\\]()]+}");
-	    Matcher m = p.matcher(expression);
-	    StringBuffer sb = new StringBuffer();
-	    while (m.find()) {
-	        String exp = m.group();
-	        String path = exp.substring(2, exp.length() - 1);
-	        Object value = getAttributeByPath(data, path);
-	        m.appendReplacement(sb, value == null ? "" : value.toString());
-	    }
-	    setCellValue(cell, sb.toString());
+	private static void fillCell(Cell cell, String expression, Object data)
+			throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+		Pattern p = Pattern.compile("\\$\\{[\\w.\\[\\]()]+}");
+		Matcher m = p.matcher(expression);
+		StringBuffer sb = new StringBuffer();
+		while (m.find()) {
+			String exp = m.group();
+			String path = exp.substring(2, exp.length() - 1);
+			Object value = getAttributeByPath(data, path);
+			m.appendReplacement(sb, value == null ? "" : value.toString());
+		}
+		setCellValue(cell, sb.toString());
 	}
-	
+
 	private static void setCellValue(Cell cell, Object value) {
-	    if (value == null) {
-	        cell.setCellValue("");
-	    } else if (value instanceof Date) {
-	        cell.setCellValue((Date) value);
-	    } else if (value instanceof Integer) {
-	        cell.setCellValue((Integer) value);
-	    } else if (value instanceof Long) {
-	        cell.setCellValue((Long) value);
-	    } else if (value instanceof Double) {
-	        cell.setCellValue((Double) value);
-	    } else if (value instanceof Float) {
-	        cell.setCellValue((Float) value);
-	    } else if (value instanceof Character) {
-	        cell.setCellValue((Character) value);
-	    } else if (value instanceof BigDecimal) {
-	        cell.setCellValue(((BigDecimal) value).doubleValue());
-	    } else {
-	        cell.setCellValue(value.toString());
-	    }
-	}
-	
-	private static Object getAttributeByPath(Object obj, String path) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
-	    String[] paths = path.split("\\.");
-	    Object o = obj;
-	    for (String s : paths) {
-	        o = PropertyUtils.getNestedProperty(o, s);
-	    }
-	    return o;
+		if (value == null) {
+			cell.setCellValue("");
+		} else if (value instanceof Date) {
+			cell.setCellValue((Date) value);
+		} else if (value instanceof Integer) {
+			cell.setCellValue((Integer) value);
+		} else if (value instanceof Long) {
+			cell.setCellValue((Long) value);
+		} else if (value instanceof Double) {
+			cell.setCellValue((Double) value);
+		} else if (value instanceof Float) {
+			cell.setCellValue((Float) value);
+		} else if (value instanceof Character) {
+			cell.setCellValue((Character) value);
+		} else if (value instanceof BigDecimal) {
+			cell.setCellValue(((BigDecimal) value).doubleValue());
+		} else {
+			cell.setCellValue(value.toString());
+		}
 	}
 
-	private static Object getAttribute(Object obj, String member) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+	private static Object getAttributeByPath(Object obj, String path)
+			throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+		String[] paths = path.split("\\.");
+		Object o = obj;
+		for (String s : paths) {
+			o = PropertyUtils.getNestedProperty(o, s);
+		}
+		return o;
+	}
+
+	private static Object getAttribute(Object obj, String member)
+			throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
 		return PropertyUtils.getNestedProperty(obj, member);
 	}
 
--- src/test/java/com/test/demo/demo/TestPoi.java
+++ src/test/java/com/test/demo/demo/TestPoi.java
@@ -1,41 +1,115 @@
 package com.test.demo.demo;
 
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootTest
 class TestPoi {
 
-	@Test
-	void test1() throws Exception {
+	//@Test
+	void ExcelTemplateUtils_test1() throws Exception {
 		Map<String, Object> data = new HashMap<>();
-        Map<String, Object> cls = new HashMap<>();
-        data.put("cls", cls);
-        cls.put("headmaster", "李景文");
-        cls.put("type", "文科班");
-        List<Stu> stus = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            Stu stu = new Stu();
-            stu.setCode("code" + i);
-            stu.setName("name" + i);
-            stus.add(stu);
-        }
-        cls.put("students", stus);
+		
+		  Map<String, Object> cls = new HashMap<>(); 
+		  data.put("cls", cls);
+		  cls.put("headmaster", "李景文"); 
+		  cls.put("type", "文科班"); 
+		  List<Stu> stus = new ArrayList<>(); 
+		  for (int i = 0; i < 10; i++) 
+		  { Stu stu = new Stu();
+		  stu.setCode("code" + i); stu.setName("name" + i); stus.add(stu); }
+		  cls.put("students", stus);
+		 
 
-        String templatePath = "/templates/excel/test.xlsx";
+        String templatePath = "/templates/excel/Disposal_Report_Template_backup.xls";
         
-        String resultFileName = File.createTempFile("test1", ".xlsx").getAbsolutePath();
+        String resultFileName = File.createTempFile("ExcelTemplateUtils_test1", ".xls").getAbsolutePath();
 		System.out.println(resultFileName);
-		FileOutputStream fos = new FileOutputStream(new File(resultFileName));
 		
         //根据模板 templatePath 和数据 data 生成 excel 文件，写入 fos 流
-        ExcelTemplateUtils.process(data, templatePath, fos);
+        byte[] bt = ExcelTemplateUtils.process(data, templatePath);
+        FileUtils.writeByteArrayToFile(new File(resultFileName), bt);
+	}
+	
+	@Test
+	void ExcelTemplate_test1() throws IOException {
+		ClassPathResource resource = new ClassPathResource("/templates/excel/Disposal_Report_ExcelTemplate.xls");
+		String filePath = resource.getFile().getAbsolutePath();
+		
+		// 加载模板表格
+		ExcelTemplate excel = new ExcelTemplate(filePath);
+		
+		// 验证是否通过
+		if (!excel.examine())
+			return;
+		try {
+			// 第一个参数，需要操作的sheet的索引
+	        // 第二个参数，需要复制的区域的第一行索引
+	        // 第三个参数，需要复制的区域的最后一行索引
+	        // 第四个参数，需要插入的位置的索引
+	        // 第五个参数，填充行区域中${}的值
+	        // 第六个参数，是否需要删除原来的区域
+	        // 需要注意的是，行的索引一般要减一
+			LinkedHashMap<Integer, LinkedList<String>> rows = ExcelTemplate_test1_getData();
+			excel.addRowByExist(0, 5, 5, 6, rows, true);
+			// 保存到指定路径
+			String resultFileName = File.createTempFile("ExcelTemplate_test1", ".xls").getAbsolutePath();
+			System.out.println(resultFileName);
+			excel.save(resultFileName);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private LinkedHashMap<Integer, LinkedList<String>> ExcelTemplate_test1_getData() {
+		// 使用一个Map来存储所有的行区域，
+        // 每个行区域对应Map的一个键
+        LinkedHashMap<Integer, LinkedList<String>> rows = new LinkedHashMap<>();
+        // 创建第一个行区域里面填充的值，ExcelTemplate会按从左至右，
+        // 从上往下的顺序，挨个填充区域里面的${}，所以创建的时候注意顺序就好
+        LinkedList<String> row1 = new LinkedList<>();
+        row1.add("1");
+        row1.add("123");
+        row1.add("张三");
+        row1.add("2019/9/10");
+        row1.add("2019/9/10");
+        row1.add("2019/9/10");
+        row1.add("5");
+        row1.add("项目上线");
+        // 把第一个行区域row1添加进入rows
+        rows.put(1,row1);
+        // 创建第二个行区域里面填充的值
+        LinkedList<String> row2 = new LinkedList<>();
+        row2.add("2");
+        row2.add("1234");
+        row2.add("李四");
+        row2.add("2019/9/11");
+        row2.add("2019/9/11");
+        row2.add("2019/9/11");
+        row2.add("6");
+        row2.add("临时突发状况");
+        // 把第二个行区域row2添加进入rows
+        rows.put(2,row2);
+
+        // 创建需要填充替换的值
+		/*
+		 * Map<String,String> fill = new HashMap<>(); fill.put("总加班时长","11");
+		 * fill.put("公司名称","xxxx有限公司"); fill.put("创建人","王麻子");
+		 * fill.put("日期","2019-9-12");
+		 */
+        
+		return rows;
 	}
 }
